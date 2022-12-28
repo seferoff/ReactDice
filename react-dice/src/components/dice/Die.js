2399e119454e8047ab1a6b20e05797e4b6efa8bb
seferoff <seferoff007@gmail.com>
2022-12-28 19:17:01 +0400

    diceisnice

---
+++ react-dice/src/components/dice/Die.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react'
+import './Die.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+class Die extends Component{
+render(){
+	const {face, rolling} = this.props
+
+	return (
+			<div>
+				<FontAwesomeIcon icon={['fas', `fa-dice-${face}`]} className={`Die
+				${rolling && 'Die-shaking'}`} />
+			</div >
+		)
+}
+}
+
+export default Die
